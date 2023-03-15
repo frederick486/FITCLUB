aeaf6fb74924fa243ee5dd2dd1b599cea7cad257
frederick486 <frederick.gaittet@orange.fr>
2023-03-15 23:45:27 +0100

    Composant <Plan />

--- FitClub-Starter/src/App.js
+++ FitClub-Starter/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import Testimonials from './assets/Testimonials/Testimonials';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Programs/>
       <Reasons/>
+      <Plans/>
+      <Testimonials/>
     </div>
   );
 }
